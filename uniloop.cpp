e203211051c6a08edc32b649345df8410b14e942
koo5 <jindrich.kolman@elektroline.cz>
2017-05-22 00:51:38 +0200

    uniloop

---
+++ uniloop.cpp
@@ -0,0 +1,14 @@
+void loop(State state, Pred* pred, std::vector<Thing *s>)
+{
+	if(state.comeback)
+		goto comeback;
+		
+
+
+for (state.rule_idx = 0; state.rule_idx < pred.size(); state.rule_idx++)
+{
+	while unify(s, rule.head.s
+		while unify(o, rule.head.o
+			for bi in rule.body:
+				loop(bi.pred, 
+	

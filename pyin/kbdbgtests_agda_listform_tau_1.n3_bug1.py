0849a992f0ba8c1b5734fffa6113a64f85c7fee3
Jindrich Kolman <kolman.jindrich@gmail.com>
2018-07-28 15:39:50 +0200

    filter

---
+++ pyin/kbdbgtests_agda_listform_tau_1.n3_bug1.py
@@ -0,0 +1,9 @@
+from query_kbdbg_with_pyin import *
+
+query(
+term(Var(), rdf.type, kbdbg.frame, ),
+term(dummy quickndirty.notIncludes :f kbdbg:is_finished true.}.
+		:kbdbg log:includes {:f kbdbg:is_for_rule :r.}.
+		:kbdbg log:includes {:b a kbdbg:binding. :b kbdbg:has_source [kbdbg:has_frame :y].}.
+		:kbdbg log:includes {:b kbdbg:has_target :t.}.
+		:kbdbg log:includes {:t kbdbg:has_frame :tf.}.

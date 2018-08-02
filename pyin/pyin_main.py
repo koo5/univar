35e7badfd77f7d454230cb418621901c32466455
Jindrich Kolman <kolman.jindrich@gmail.com>
2018-08-02 22:23:41 +0200

    sparqls

--- pyin/pyin_main.py
+++ pyin/pyin_main.py
@@ -10,41 +10,68 @@ server.update("""CLEAR GRAPHS""")
 
 server.update("""
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
-INSERT DATA
-{ 
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix kbdbg: <http://kbd.bg/#> .
-@prefix : <file:///#> .
-_:x rdf:type rdf:Statement .
-_:x rdf:subject :Tolkien .
-_:x rdf:predicate :wrote .
-_:x rdf:object :LordOfTheRings .
-_:x <http://kbd.bg/#has_step> 1.
-}
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX kbdbg: <http://kbd.bg/#> 
+PREFIX : <file:///#> 
+INSERT 
+{
+	Graph :step1
+	{ 
+		:Tolkien :wrote :LordOfTheRings. 
+	}.
+	Graph :step2
+	{ 
+		:LordOfTheRings :is :boring. 
+	}.
+	Graph :step3
+	{ 
+		:LordOfTheRings :is :exciting. 
+	}.
+	
+} WHERE {}
 """)
 
+result = server.query("""
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX kbdbg: <http://kbd.bg/#>
+PREFIX : <file:///#>
+SELECT * 
+FROM :step1
+FROM :step2
+WHERE
+{
+	:Tolkien :wrote ?what. ?what :is ?rating.
+}
+""")#?what :is ?rating.
+print ('yo')
+print(result)
 server.update("""
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
-INSERT DATA
-{ 
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix kbdbg: <http://kbd.bg/#> .
-@prefix : <file:///#> .
-_:x rdf:type rdf:Statement .
-_:x rdf:subject :Tolkien .
-_:x rdf:predicate :wrote .
-_:x rdf:object :LordOfTheRings .
-_:x <http://kbd.bg/#has_step> 1.
-}
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX kbdbg: <http://kbd.bg/#> 
+PREFIX : <file:///#> 
+INSERT 
+{
+	:step1 rdf:a :result. :step2 rdf:a :result.
+} WHERE {}
 """)
 
-
-
-
-result = server.query('select * where { ?s ?p ?o }')
-for b in result['results']['bindings']:
-    print ("%s %s %s"% (b['s']['value'], b['p']['value'], b['o']['value']))
-
+result = server.query("""
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX kbdbg: <http://kbd.bg/#>
+PREFIX : <file:///#>
+SELECT * 
+FROM ?result
+WHERE
+{
+	?result rdf:a :result. :Tolkien :wrote ?what. ?what :is ?rating.
+}
+""")#?what :is ?rating.
+print ('yo')
+print(result)
+exit()
 import rdflib
 from ordered_rdflib_store import OrderedStore
 

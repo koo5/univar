f56f4067ff61e8abcaab54e0c4db96e306c87b05
Your Name <you@example.com>
2019-02-28 16:33:51 +0100

    wth

---
+++ kbdbg2jsonld/x.js
@@ -0,0 +1,14 @@
+var done = 0;
+(function wait () {if (!done) setTimeout(wait, 1000);})();
+
+const jsonld = require('jsonld');
+
+const line = "<file://bn614X>  <http://www.w3.org/1999/02/22-rdf-syntax-ns#first> <http://www.w3.org/1999/02/22-rdf-syntax-ns#nil>."
+jsonld.fromRDF(line, {format: 'application/n-quads'}, (err, doc) => {
+            if (err) {
+              console.log("err" + err)
+            } else {
+              console.log("doc"+doc)
+            }
+            done = true
+});
